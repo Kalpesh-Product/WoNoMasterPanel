595581e95731d5539430e84c1a6e91fcc53fe33f
sankalp-wono <sankalp.wono@gmail.com>
2025-09-12 12:18:58 +0530

    some changes

--- client/src/pages/Dashboard/FrontendDashboard/NomadListingsOverview.jsx
+++ client/src/pages/Dashboard/FrontendDashboard/NomadListingsOverview.jsx
@@ -66,7 +66,7 @@ export default function NomadListingsOverview() {
           search
           tableTitle="Nomad Listings"
           loading={isPending}
-          buttonTitle="Add Listing"
+          buttonTitle="Add Product"
           handleClick={handleAddClick}
         />
       </PageFrame>
