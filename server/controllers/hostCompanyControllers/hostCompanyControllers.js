1bbe4ed682538d9356e18c5086e597ca01a6c854
muskan-wono <muskan.wono@gmail.com>
2025-09-26 12:16:23 +0530

    update services controller

--- server/controllers/hostCompanyControllers/hostCompanyControllers.js
+++ server/controllers/hostCompanyControllers/hostCompanyControllers.js
@@ -114,76 +114,6 @@ const serviceOptions = [
 const validApps = new Set(serviceOptions[0].items);
 const validModules = new Set(serviceOptions[1].items);
 
-const requestServices = async (req, res, next) => {
-  try {
-    const { companyId, requestedServices = {} } = req.body;
-
-    if (!companyId) {
-      return res.status(400).json({
-        message: "companyId is required",
-      });
-    }
-
-    // default to empty arrays if not provided
-    const { apps = [], modules = [] } = requestedServices;
-
-    // if both are missing/empty, reject
-    if (!apps.length && !modules.length) {
-      return res.status(400).json({
-        message: "At least one of apps or modules must be provided",
-      });
-    }
-
-    // validate incoming apps/modules
-    const invalidApps = apps.filter((a) => !validApps.has(a.appName));
-    const invalidModules = modules.filter(
-      (m) => !validModules.has(m.moduleName)
-    );
-
-    if (invalidApps.length || invalidModules.length) {
-      return res.status(400).json({
-        message: "Invalid app/module names provided",
-        invalidApps: invalidApps.map((a) => a.appName),
-        invalidModules: invalidModules.map((m) => m.moduleName),
-      });
-    }
-
-    const company = await HostCompany.findOne({ companyId });
-    if (!company) throw new Error("Company not found");
-
-    const appsMap = new Map(
-      company.selectedServices.apps.map((a) => [a.appName, a])
-    );
-    const modulesMap = new Map(
-      company.selectedServices.modules.map((m) => [m.moduleName, m])
-    );
-
-    apps.forEach((app) => {
-      if (appsMap.has(app.appName)) {
-        appsMap.get(app.appName).isRequested = true;
-      } else {
-        company.selectedServices.apps.push(app);
-      }
-    });
-
-    modules.forEach((mod) => {
-      if (modulesMap.has(mod.moduleName)) {
-        modulesMap.get(mod.moduleName).isRequested = true;
-      } else {
-        company.selectedServices.modules.push(mod);
-      }
-    });
-
-    await company.save();
-
-    return res.status(200).json({
-      message: "Request submitted successfully",
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
 const updateServices = async (req, res, next) => {
   try {
     const { companyId, selectedServices = {} } = req.body;
@@ -413,7 +343,6 @@ module.exports = {
   createCompany,
   activateProduct,
   updateServices,
-  requestServices,
   getCompanies,
   getCompany,
   bulkInsertCompanies,
--- server/routes/hostCompanyRoutes.js
+++ server/routes/hostCompanyRoutes.js
@@ -8,7 +8,6 @@ const {
   bulkInsertCompanies,
   updateServices,
   activateProduct,
-  requestServices,
 } = require("../controllers/hostCompanyControllers/hostCompanyControllers");
 const {
   getAllCompanyListings,
@@ -26,7 +25,6 @@ router.post(
 );
 router.post("/onboard-company", createCompany);
 router.patch("/activate-product", activateProduct);
-router.patch("/request-services", requestServices);
 router.patch("/update-services", updateServices);
 router.get("/companies", getCompanies);
 router.get("/company", getCompany);
