83508650bc979b34e6c9fbc9846a1a92a148323f
allanwono <allan.wono@gmail.com>
2026-01-30 10:53:16 +0530

    Advanced Filter Responsiveness In Phone View

--- client/src/components/MuiAside.jsx
+++ client/src/components/MuiAside.jsx
@@ -1,5 +1,5 @@
-import { Drawer, IconButton } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
+import { Drawer, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const MuiAside = ({ open, onClose, children, title }) => {
   return (
@@ -7,16 +7,16 @@ const MuiAside = ({ open, onClose, children, title }) => {
       anchor="right"
       open={open}
       onClose={onClose}
-      PaperProps={{ className: 'bg-gray-100 w-[600px]' }}
+      PaperProps={{ className: "bg-gray-100 w-screen sm:w-[600px]" }}
     >
       {/* Close Button */}
       <div className="flex items-center justify-between p-3 border-b-default border-borderGray">
         <IconButton
           onClick={onClose}
           sx={{
-            backgroundColor: '#e5e7eb',
-            color: '#374151',
-            '&:hover': { backgroundColor: '#d1d5db', color: '#111827' },
+            backgroundColor: "#e5e7eb",
+            color: "#374151",
+            "&:hover": { backgroundColor: "#d1d5db", color: "#111827" },
           }}
         >
           <CloseIcon fontSize="small" />
@@ -27,7 +27,7 @@ const MuiAside = ({ open, onClose, children, title }) => {
 
       {/* Content */}
       <div className="flex flex-col items-center justify-start h-screen gap-4">
-        <div className='w-full p-4'>{children}</div>
+        <div className="w-full p-4">{children}</div>
       </div>
     </Drawer>
   );
