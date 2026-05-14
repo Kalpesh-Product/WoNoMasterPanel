acadeaa6dea7b32cf17c9cd55fd0ed1042840fda
pranav-wono <pranavv.wono@gmail.com>
2026-05-14 14:52:40 +0530

    feat: implement signup leads dashboard with lead management, status tracking, and automated invite emails

--- server/routes/hostUserRoutes.js
+++ server/routes/hostUserRoutes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
-const { bulkInsertPoc } = require("../controllers/hostUserControllers");
+const verifyJwt = require("../middlewares/verifyJwt");
+const {
+  bulkInsertPoc,
+  sendInviteEmail,
+} = require("../controllers/hostUserControllers");
 
 router.post("/bulk-insert-poc", bulkInsertPoc);
+router.post("/send-invite", verifyJwt, sendInviteEmail);
 
 module.exports = router;
