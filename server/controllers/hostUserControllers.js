8790bdc21cc0c014035d728ac0be66f21519faed
allanwono <allan.wono@gmail.com>
2026-09-16 11:41:33 +0530

    changes

--- server/controllers/hostUserControllers.js
+++ server/controllers/hostUserControllers.js
@@ -1992,11 +1992,7 @@ const bulkInsertPoc = async (req, res, next) => {
     }
 
     const companyIds = [
-      ...new Set(
-        pocs
-          .map((poc) => poc?.companyId?.trim())
-          .filter(Boolean),
-      ),
+      ...new Set(pocs.map((poc) => poc?.companyId?.trim()).filter(Boolean)),
     ];
 
     // Fetch only companies referenced by this upload.
